893b3fd4f58397f6e162cdffab4582fd91da8a54
akprajapati01 <ap312863@gmail.com>
2023-11-27 21:49:45 +0530

    Input from user based calculator added

---
+++ simple calculator.py
@@ -0,0 +1,6 @@
+a=int(input("Input First Number:"))
+b=int(input("Input Second Number:"))
+print("Addition :",a+b)
+print("Substraction :",a-b)
+print("Multiplication:",a*b)
+print("Division :",a/b)
