af10e729e9b2d897e8fa72aa871e0fdafdb03117
akprajapati01 <ap312863@gmail.com>
2023-12-05 21:58:43 +0530

    Files Updated

---
+++ loop.py
@@ -0,0 +1,12 @@
+# fruits=["Apple","Guava","Pineapple","Orange"]
+# for i in fruits:
+#     print(i)
+
+# for k in range(10):
+#     print(k+1)
+
+fact=1
+num=int(input())
+for k in range(num):
+    fact=1+fact*k                # Reading input from STDIN
+print(fact)   
