23722f08a7f597ab64b03070a2e552403c6801c6
akprajapati01 <ap312863@gmail.com>
2024-01-04 16:02:57 +0530

    Set methods Added

---
+++ set mrthod.py
@@ -0,0 +1,8 @@
+s1={1,3,5,2,6,2,5}
+s2={4,26,25,82,2,1,3,7}
+print(s1.union(s2))
+#s1.update(s2)
+print(s1.intersection(s2))
+# s1.intersection_update(s2)
+print(s1.symmetric_difference(s2))
+print(s1.isdisjoint(s2))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
-elif(hour>=12 and  hour<16):
-    print("Good Afternoon Sir!")
-elif(hour>=16 and  hour<0):
-    print("Good Evening Sir!")
+s1.update(s2)
