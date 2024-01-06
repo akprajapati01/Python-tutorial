32ecb6f31ac9b0fd6e86d339b313f5df7365e220
akprajapati01 <ap312863@gmail.com>
2024-01-06 18:57:01 +0530

    Dictonary Method Updated

---
+++ dictonary method.py
@@ -0,0 +1,5 @@
+dict={1:"Abhishek",2:"Amit",3:"Deepak",4:"Alok"}
+dict2={"Abhishek":"Civil Engineer","Alok":"Computer Science Engineer","Apple":"Fruits","Fortuner":"Car"}
+dict.update(dict2)
+dict.popitem()
+print(dict)
