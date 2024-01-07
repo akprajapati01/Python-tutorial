5aecb6a19f6c7bd1f4fc4b6d568116b44a45f59c
akprajapati01 <ap312863@gmail.com>
2024-01-07 19:07:49 +0530

    Files Updated

---
+++ for-with-else.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    print(i)
+else:
+    print("Sorry not in range")
+
+for i in []:
+    print(i)
+else:
+    print("Sorry not in Set")
+
+for i in range(10):
+    print(i)
+    if(i==4):
+        break
+else:
+    print("Sorry not in range")
