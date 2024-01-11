b59b1cc55569ccd770b6d3d1d4569720584e14a9
akprajapati01 <ap312863@gmail.com>
2024-01-11 15:02:46 +0530

    Files Are Modified

--- custom error.py
+++ custom error.py
@@ -1,3 +1,9 @@
-n=int(input("Input any number between 5 and 50:"))
-if(n<5 or n>50):
-    raise ValueError("Number is not between 5 and 50")
+# n=int(input("Input any number between 5 and 50:"))
+# if(n<5 or n>50):
+#     raise ValueError("Number is not between 5 and 50")
+
+a=input("What is Your Name ?")
+if(a=="Abhishek"):
+    raise ValueError("Your the Develope")
+else:
+    print(f"Hi {a} How are You I hope you are Doing Well in your Life")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
-s1.update(s2)
+n=int(input("Input any number between 5 and 50:"))
+# if(n<5 or n>50):
+#     raise ValueError("Number is not between 5 and 50")
