8eab6af16134df612b3b44520cf76b2f910846f1
akprajapati01 <ap312863@gmail.com>
2024-01-12 13:49:07 +0530

    KBC Project Updated

---
+++ KBC Season2.py
@@ -0,0 +1,6 @@
+questions=[["1.What is my Name ?","a.)Rahul", "b.)Abhishek", "c.)Amit", "d.)Deepak",2],["2.What is your Country Name ?","a.)India", "b.)Nepal," "c.)USA,", "d.)Brazil"]]
+levels=[1000,2000,5000,10000,25000,30000,50000,100000,125000,200000,250000]
+
+for i in range(0,len(question)):
+    question=questions[i]
+    print(f"Question for 1000")
