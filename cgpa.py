81330d26f634d9e39431f62af0ac153264954943
akprajapati01 <ap312863@gmail.com>
2024-06-01 10:19:43 +0530

    File Modified

--- cgpa.py
+++ cgpa.py
@@ -1,5 +1,12 @@
 m1 = int(input("Input marks of paper 1 : "))
-m1 = int(input("Input marks of paper 2 : "))
-m1 = int(input("Input marks of paper 3 : "))
-m1 = int(input("Input marks of paper 4 : "))
-m1 = int(input("Input marks of paper 5 : "))
+m2 = int(input("Input marks of paper 2 : "))
+m3 = int(input("Input marks of paper 3 : "))
+m4 = int(input("Input marks of paper 4 : "))
+m5 = int(input("Input marks of paper 5 : "))
+
+#For Practicle marks
+m1 = int(input("Input marks of practicle paper 1 : "))
+m2 = int(input("Input marks of practicle paper 2 : "))
+m3 = int(input("Input marks of practicle paper 3 : "))
+m4 = int(input("Input marks of practicle paper 4 : "))
+m5 = int(input("Input marks of practicle paper 5 : "))
