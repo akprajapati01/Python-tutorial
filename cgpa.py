5c9bf3c8b0ac7ae64172e41e36840ed0fa010547
akprajapati01 <ap312863@gmail.com>
2024-06-04 16:24:34 +0530

    File Modified

--- cgpa.py
+++ cgpa.py
@@ -1,16 +1,30 @@
 sem = int(input("Input The Semester for whhich you want to calculate the cgpa : "))
 if(sem <=8 and sem > 0):
-    m1 = int(input("Input marks of paper 1 : "))
-    m2 = int(input("Input marks of paper 2 : "))
-    m3 = int(input("Input marks of paper 3 : "))
-    m4 = int(input("Input marks of paper 4 : "))
-    m5 = int(input("Input marks of paper 5 : "))
+    if(sem == 1):
+        m1 = int(input("Input marks of paper 1 : "))
+        m2 = int(input("Input marks of paper 2 : "))
+        m3 = int(input("Input marks of paper 3 : "))
+        m4 = int(input("Input marks of paper 4 : "))
+        m5 = int(input("Input marks of paper 5 : "))
 
-    #For Practicle marks
-    m1 = int(input("Input marks of practicle paper 1 : "))
-    m2 = int(input("Input marks of practicle paper 2 : "))
-    m3 = int(input("Input marks of practicle paper 3 : "))
-    m4 = int(input("Input marks of practicle paper 4 : "))
-    m5 = int(input("Input marks of practicle paper 5 : "))
+        #For Practicle marks
+        m1 = int(input("Input marks of practicle paper 1 : "))
+        m2 = int(input("Input marks of practicle paper 2 : "))
+        m3 = int(input("Input marks of practicle paper 3 : "))
+        m4 = int(input("Input marks of practicle paper 4 : "))
+        m5 = int(input("Input marks of practicle paper 5 : "))
+    elif(sem == 1):
+        m1 = int(input("Input marks of paper 1 : "))
+        m2 = int(input("Input marks of paper 2 : "))
+        m3 = int(input("Input marks of paper 3 : "))
+        m4 = int(input("Input marks of paper 4 : "))
+        m5 = int(input("Input marks of paper 5 : "))
+
+        #For Practicle marks
+        m1 = int(input("Input marks of practicle paper 1 : "))
+        m2 = int(input("Input marks of practicle paper 2 : "))
+        m3 = int(input("Input marks of practicle paper 3 : "))
+        m4 = int(input("Input marks of practicle paper 4 : "))
+        m5 = int(input("Input marks of practicle paper 5 : "))
 else:
     print("Input Correct Semester Number ")
