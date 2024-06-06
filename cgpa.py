127d32eb4336905a8054788ea46c4ac099c9c1e3
akprajapati01 <ap312863@gmail.com>
2024-06-06 09:11:24 +0530

    File Modified

--- cgpa.py
+++ cgpa.py
@@ -4,15 +4,12 @@ if(sem <=8 and sem > 0):
         m1 = int(input("Input marks of paper 1 : "))
         m2 = int(input("Input marks of paper 2 : "))
         m3 = int(input("Input marks of paper 3 : "))
-        m4 = int(input("Input marks of paper 4 : "))
-        m5 = int(input("Input marks of paper 5 : "))
 
         #For Practicle marks
         m1 = int(input("Input marks of practicle paper 1 : "))
         m2 = int(input("Input marks of practicle paper 2 : "))
         m3 = int(input("Input marks of practicle paper 3 : "))
-        m4 = int(input("Input marks of practicle paper 4 : "))
-        m5 = int(input("Input marks of practicle paper 5 : "))
+
     elif(sem == 2):
         m1 = int(input("Input marks of paper 1 : "))
         m2 = int(input("Input marks of paper 2 : "))
