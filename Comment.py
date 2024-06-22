a2bdf1546d81b69265502804edc28bb509809c10
Abhishek Kumar Prajapati <137279564+akprajapati01@users.noreply.github.com>
2024-06-22 22:14:30 +0530

    Update Comment.py

--- Comment.py
+++ Comment.py
@@ -8,4 +8,4 @@ This is another type of multi line comment used in python
 # this is single line comment
 
 print("hi I am abhishek " , 5,7,8, sep="%", end="006")
-print("hi boy what are you \n what are doing")
+print("hi boy what are you doing in your life \n what are doing")
