b92d5d1c883fa588fc64ec4a5b9daa154e1cd3e4
Abhishek Kumar Prajapati <137279564+akprajapati01@users.noreply.github.com>
2024-07-01 23:24:51 +0530

    Update KBC Season2.py

--- KBC Season2.py
+++ KBC Season2.py
@@ -1,4 +1,4 @@
-questions=[["1.What is my Name ?","a.)Rahul", "b.)Abhishek", "c.)Amit", "d.)Deepak",2],["2.What is your Country Name ?","a.)India", "b.)Nepal," "c.)USA,", "d.)Brazil"]]
+questions=[["1.What is my Name ?","a.)Sonu", "b.)Abhishek", "c.)Amit", "d.)Shankar",2],["2.What is your Country Name ?","a.)India", "b.)Nepal," "c.)USA,", "d.)Brazil"]]
 levels=[1000,2000,5000,10000,25000,30000,50000,100000,125000,200000,250000]
 
 for i in range(0,len(questions)):
